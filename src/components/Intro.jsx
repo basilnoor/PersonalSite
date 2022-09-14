e120fe5aa3d6d1e7bcb49ed8c4bdfa95781dbbe2
Basil Noor <basilnoor5@gmail.com>
2022-09-14 00:32:00 -0400

    fixed intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax'
 
 import im from "../assets/intro/I'm.svg"
 import name from "../assets/intro/Basil.svg"
-import imA from "../assets/intro/I'm_a.svg"
+import A from "../assets/intro/A.svg"
 import title from "../assets/intro/Software_Developer.svg"
 
 const Intro = () => {
@@ -17,8 +17,8 @@ const Intro = () => {
                     <ParallaxBannerLayer translateY={[40, 55]} translateX={[55, 65]} speed={5} shouldAlwaysCompleteAnimation='true' expanded='false'>
                         <img className='md:w-28' src={name} alt='' />
                     </ParallaxBannerLayer>
-                    <ParallaxBannerLayer translateY={[48, 73]} translateX={[30, 50]} speed={5} shouldAlwaysCompleteAnimation='true' expanded='false'>
-                        <img src={imA} alt='' />
+                    <ParallaxBannerLayer translateY={[47, 75]} translateX={[35, 35]} speed={1} shouldAlwaysCompleteAnimation='true' expanded='false'>
+                        <img src={A} alt='' />
                     </ParallaxBannerLayer>
                     <ParallaxBannerLayer translateY={[50, 75]} translateX={[35, 25]} speed={5} shouldAlwaysCompleteAnimation='true' expanded='false'>
                         <img className='sm:w-56 md:w-96 lg:w-auto' src={title} alt='' />
