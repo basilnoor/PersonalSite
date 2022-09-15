29a9e2385792bb2ae0dcb042fc69b40ed9e13e24
Basil Noor <basilnoor5@gmail.com>
2022-09-14 21:39:49 -0400

    para

--- src/components/ParallaxBg.jsx
+++ src/components/ParallaxBg.jsx
@@ -6,10 +6,10 @@ import BgImg from '../assets/paraGoku.jpg'
 const ParallaxBg = () => {
     return (
         <div >
-            <div className='bg-tertiary h-10'></div>
+            <div className='bg-tertiary h-10' />
             <Parallax className='absolute bg-white sm:h-[15vh] sm:pt-20 md:h-[20vh] md:pt-18 lg:h-[25vh] lg:pt-0'
                 bgImage={BgImg} bgImageAlt='parallaxImage' strength={400} />
-            <div className='bg-tertiary h-10'></div>
+            <div className='bg-tertiary h-10' />
         </div>
     )
 }
